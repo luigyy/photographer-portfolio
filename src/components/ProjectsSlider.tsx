bac63fb404aefa5eca8033395228f266447bf730
luigyy <luigy.valverde@ucr.ac.cr>
2023-02-13 21:23:24 -0600

    slider kinda working

--- src/components/ProjectsSlider.tsx
+++ src/components/ProjectsSlider.tsx
@@ -1,27 +1,39 @@
+import "./slider.css";
 interface ProjectsSliderProps {}
 
 const ProjectsSlider: React.FC<ProjectsSliderProps> = ({}) => {
-  function moveRight() {
+  var slide = 10;
+  var current = 0;
+  function move(direction: "left" | "right") {
     const slider = document.getElementById("slider");
     if (slider) {
-      slider.style.transform = "transform(100px)";
-      slider.style.border = "10px solid red";
+      direction === "left"
+        ? (slider.style.transform = `translate(${current + slide}%)`)
+        : (slider.style.transform = `translate(${current - slide}%)`);
     }
+    console.log(current);
+    direction === "right"
+      ? (current = Math.max((current -= slide), -50))
+      : (current = Math.min(((current += slide), 0)));
   }
 
   return (
-    <div className="relative h-72 bg-[#2d2a24] font-main">
+    <div className="relative h-72 overflow-x-clip bg-[#2d2a24] font-main">
       <div className="flex items-center justify-between">
         <h1 className="px-2 pl-28 pt-14 text-xl text-[#e4b350]/90">
           — My Recent Projects
         </h1>
         <div className="flex gap-2 pt-14 pr-28">
-          <span id="button-right" className="rounded-full text-4xl text-white">
+          <button
+            id="button-right"
+            className="rounded-full text-4xl text-white"
+            onClick={() => move("left")}
+          >
             ←
-          </span>
+          </button>
           <button
             className="rounded-full text-4xl text-white"
-            onClick={moveRight}
+            onClick={() => move("right")}
           >
             →
           </button>
@@ -31,7 +43,7 @@ const ProjectsSlider: React.FC<ProjectsSliderProps> = ({}) => {
       <div className="absolute mt-20 h-80 min-w-full">
         <div
           id="slider"
-          className="test flex h-full w-full translate-x-28 gap-4 border-2 "
+          className="flex h-full w-full translate-x-28 gap-4  border-2 "
         >
           <div className="h-full w-64">
             <img
